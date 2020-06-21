7aa8c17c8478b0c83f7802cfef03245c73cac1c5
Diego <luquesdiego@outlook.com>
2020-06-21 10:57:46 -0400

    Entendendo os Observers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,35 @@ export class AppComponent implements OnInit {
   title = 'AngularRxjs';
 
   ngOnInit(): void {
+    // Chamada sem tratamento de erro
     // this.minnhaPromisse('Diego')
     //   .then(result => console.log('Resultado:', result))
 
+    // Chamada com tratamento de erro
     // this.minnhaPromisse('Diogo')
     //   .then(result => console.log('Resultado:', result))
     //   .catch(erro => console.log('Ocorreu um erro:', erro)
     //   )
 
-    this.minhaObservable('')
+    // Chamada com tratamento de erro
+    this.minhaObservable('Diego')
       .subscribe(
         result => console.log('Resultado: ', result),
-        erro => console.log('Erro: ', erro)
-      )
+        erro => console.log('Erro: ', erro),
+        () => console.log('Fim')
+      );
+
+    //Inicío: Criar observer mais complexo caso necessário
+    //const observer = {
+    //  next: valor => console.log('Next: ', valor),
+    //  error: erro => console.log('Next: ', erro),
+    //  complete: () => console.log('Fim')
+    //}
+
+    //const obs = this.minhaObservable('Diego');
+    //obs.subscribe(observer);
+    //Fim
+
   }
 
   minnhaPromisse(nome: string): Promise<string> {
@@ -45,6 +61,7 @@ export class AppComponent implements OnInit {
         setTimeout(() => {
           subscriber.next('Resposta com delay ' + nome);
         }, 5000);
+        // subscriber.complete(); //Corta a comunicação com a observable
       }
       else {
         subscriber.error('Ops! Deu erro!');
