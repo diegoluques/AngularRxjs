0a7cb772e44c685df2f4170bf5bc2338321fc5f2
Diego <luquesdiego@outlook.com>
2020-06-21 10:32:04 -0400

    Minha primeira Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,15 @@ export class AppComponent implements OnInit {
     // this.minnhaPromisse('Diego')
     //   .then(result => console.log('Resultado:', result))
 
-    this.minnhaPromisse('Diogo')
-      .then(result => console.log('Resultado:', result))
-      .catch(erro => console.log('Ocorreu um erro:', erro)
+    // this.minnhaPromisse('Diogo')
+    //   .then(result => console.log('Resultado:', result))
+    //   .catch(erro => console.log('Ocorreu um erro:', erro)
+    //   )
+
+    this.minhaObservable('')
+      .subscribe(
+        result => console.log('Resultado: ', result),
+        erro => console.log('Erro: ', erro)
       )
   }
 
@@ -26,6 +33,21 @@ export class AppComponent implements OnInit {
       }
       else {
         reject('Ops! Você não é Diego');
+      }
+    })
+  }
+
+  minhaObservable(nome: string): Observable<string> {
+    return new Observable(subscriber => {
+      if (nome === 'Diego') {
+        subscriber.next('Olá ' + nome);
+        subscriber.next('Olá de novo ' + nome);
+        setTimeout(() => {
+          subscriber.next('Resposta com delay ' + nome);
+        }, 5000);
+      }
+      else {
+        subscriber.error('Ops! Deu erro!');
       }
     })
   }
